43b8e273a78eeb12f6fe582f2b5aaab6d1cbd919
dhenyfferarantes <dhenyffer.arantes@estudante.ufla.br>
2026-08-03 17:17:18 -0300

    testee

---
+++ src/main/java/com/example/sistema_tribus/SistemaTribusApplication.java
@@ -0,0 +1,14 @@
+package com.example.sistema_tribus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTribusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTribusApplication.class, args);
+	}
+
+	
+}
